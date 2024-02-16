7681b65aa52226e23a05030623d93d62b22f0d90
DarwinML2 <darwinmass.ml2@gmail.com>
2024-02-15 23:02:26 -0400

    add data price and title

---
+++ src/data/pizzas.js
@@ -0,0 +1,34 @@
+export const pizzas = [
+    {
+        label: "Pizza Margherita",
+        price:10
+    },
+    {
+        label: "Pizza Margherita",
+        price:10
+    },
+    {
+        label: "Pizza Margherita",
+        price:10
+    },
+    {
+        label: "Pizza Margherita",
+        price:10
+    },
+    {
+        label: "Pizza Margherita",
+        price:10
+    },
+    {
+        label: "Pizza Margherita",
+        price:10
+    },
+    {
+        label: "Pizza Margherita",
+        price:10
+    },
+    {
+        label: "Pizza Margherita",
+        price:10
+    }
+]
